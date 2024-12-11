14039617058e05aa1ae88f39a161ba1f0cd40bd4
Tayyebah28 <tayyebah28@gmail.com>
2024-12-11 12:39:22 -0500

    Add files via upload

---
+++ assets/java script/main.js
@@ -0,0 +1,97 @@
+
+let btn = document.getElementById('addbtn');
+btn.addEventListener('click', function(){
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+   num1 =  parseInt(num1);
+    num2 = parseInt(num2);
+    r = num1 + num2
+    document.getElementById('result').value = r
+    
+})
+
+let sub = document.getElementById('subbtn');
+sub.addEventListener('click', function(){
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+   num1 =  parseInt(num1);
+    num2 = parseInt(num2);
+    r = num1 - num2
+    document.getElementById('result').value = r
+    
+})
+
+
+
+let multiply = document.getElementById('multiplybtn');
+multiply.addEventListener('click', function(){
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+   num1 =  parseInt(num1);
+    num2 = parseInt(num2);
+     let r = num1 * num2
+    document.getElementById('result').value = r
+    
+})
+
+let divide = document.getElementById('divbtn');
+divide.addEventListener('click', function(){
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+   num1 =  parseInt(num1);
+    num2 = parseInt(num2);
+    r = num1 / num2
+    document.getElementById('result').value = r
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let t=10
+window.setInterval(function(){
+    document.getElementById('time').innerText = t
+    t--
+},2000)
+
+
+
+let date = new Date();
+document.getElementById('time').innerText =  date.getHours()+' : '+ date.getMinutes()+" : "+date.getSeconds();
+
+
+
+let button = document.getElementById('cross');
+button.addEventListener('click', function(){
+ document.getElementById('layer').style.display= 'none'
+})
+
+let icon= document.getElementById('bell');
+icon.addEventListener('click', function(){
+    document.getElementById('layer').style.display= 'block'
+})
+
+
+let text= document.getElementById('testing');
+text.addEventListener('click', function(){
+    let date = new Date();
+    text.innerText = date.getHours()+' ; '+ date.getMinutes();
+})
+let heading= document.getElementById('test2');
+heading.addEventListener('click', function(){
+    heading.innerHTML=  "<h4> testing java script</h4>";
+})
+console.log(Date())
